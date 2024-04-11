00ed427ba326eba5fc2d1aacb23c114adad7ff9b
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-04-11 13:15:08 +0300

    add files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    
+    .then(books => renderBooks(books))
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
 }
 
 function renderBooks(books) {
